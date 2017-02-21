4aaab9590588d74b1264e8ce846ab2a3dea8f445
usesgraphcrt <usesgraphcrt@gmail.com>
2017-02-21 15:56:46 +0500

    bag fixes

--- models/search/ServiceMiniSearch.php
+++ models/search/ServiceMiniSearch.php
@@ -5,12 +5,12 @@ namespace halumein\servicemini\models\search;
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use halumein\servicemini\models\MiniService;
+use halumein\servicemini\models\ServiceMini;
 
 /**
  * MiniServiceSearch represents the model behind the search form about `app\models\MiniService`.
  */
-class MiniServiceSearch extends MiniService
+class ServiceMiniSearch extends ServiceMini
 {
     /**
      * @inheritdoc
@@ -41,7 +41,7 @@ class MiniServiceSearch extends MiniService
      */
     public function search($params)
     {
-        $query = MiniService::find();
+        $query = ServiceMini::find();
 
         // add conditions that should always apply here
 
